8f48e806ed8f0ebdb529cc289b3b6edd9ff42c5b
dev-kahgehtan <kahgeh.tan@xero.com>
2018-11-26 14:19:19 +1100

    generates qr code based on text

---
+++ src/app/src/main/java/com/coffeebuz/coffeebuz/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.coffeebuz.coffeebuz
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import com.github.sumimakito.awesomeqr.AwesomeQRCode
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onGenerateQRCodeClick( view: View){
+        val qrCodeText = findViewById<EditText>(R.id.qrText)
+        AwesomeQRCode.Renderer().contents(qrCodeText!!.text.toString())
+            .size(800).margin(20)
+            .renderAsync(object : AwesomeQRCode.Callback {
+                override fun onRendered(renderer: AwesomeQRCode.Renderer, bitmap: Bitmap) {
+                    // Tip: here we use runOnUiThread {...} to avoid the problems caused by operating UI elements from a non-UI thread.
+                    runOnUiThread {
+                        val qrCodeImageViewer = findViewById<ImageView>(R.id.qrCode)
+                        qrCodeImageViewer!!.setImageBitmap(bitmap)
+                    }
+                }
+
+                override fun onError(renderer: AwesomeQRCode.Renderer, e: Exception) {
+                    e.printStackTrace()
+                }
+            })
+    }
+}
